9ff8a644c645c75904dba4ab8ece57dc651e005f
TG Expert☆☆☆☆☆ <78008481+v-v-r-official@users.noreply.github.com>
2021-03-07 20:02:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("⭕️CHANNEL⭕️", url="https://t.me/VKPROJECTS")],
         [InlineKeyboardButton(
-            "⭕️GROUP⭕️", url="https://t.me/VKP_BOTS")]
+            "⭕️GROUP⭕️", url="https://t.me/VKP_BOTS), InlineKeyboardButton(text="♐️SHARE♐️", url="tg://msg?text=Hai%20Friend+❤️,+Today%20i+just+found+out+an+intresting+and+Powerful+**YouTube+Bot**+for+Free🥰.+**Bot+Link**+:+@YouTubeVKbot+🔥")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n**I'm A POWERFULL YOUTUBE DOWNLOADER Bot💯\nPlease send me any YOUTUBE link,\nClick /help for more detailS..\nYou must subscribe our channel in order to use me😇**"
     await message.reply_text(welcomed, reply_markup=joinButton)
