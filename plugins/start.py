ea1fc9fd6204ee96484b64757958d8f8c6528ef3
TG Expert☆☆☆☆☆ <78008481+v-v-r-official@users.noreply.github.com>
2021-03-07 19:54:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -4,9 +4,11 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
     # return
-    joinButton = InlineKeyboardMarkup([[InlineKeyboardButton("⭕️CHANNEL⭕️", url="https://t.me/VKPROJECTS")],
-                                       [InlineKeyboardButton("⭕️GROUP⭕️", url="https://t.me/VKP_BOTS")],InlineKeyboardButton("♐️SHARE♐️", url="tg://msg?text=Hai%20Friend+❤️,+Today%20i+just+found+out+an+intresting+and+Powerful+**YouTube+Bot**+for+Free🥰.+**Bot+Link**+:+@YouTubeDL_VideoBot+🔥")]]),
-
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("⭕️CHANNEL⭕️", url="https://t.me/VKPROJECTS")],
+        [InlineKeyboardButton(
+            "⭕️GROUP⭕️", url="https://t.me/VKP_BOTS")]
+    ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n**I'm A POWERFULL YOUTUBE DOWNLOADER Bot💯\nPlease send me any YOUTUBE link,\nClick /help for more detailS..\nYou must subscribe our channel in order to use me😇**"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
